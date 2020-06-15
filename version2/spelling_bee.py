c00bcc9888ebb6c397d8f85c86dcf0df6d29b27b
Daniel Cohen <dbcohen7260@gmail.com>
2020-06-15 08:39:10 -0600

    version2 upload

---
+++ version2/spelling_bee.py
@@ -0,0 +1,308 @@
+"""
+Created on Thu Jun 11 15:43:20 2020
+
+@author: drahcir1
+"""
+
+# imports for getting gameData
+from lxml import html
+import requests, time, json 
+
+#import nltk
+#nltk.download('words')
+
+# list of complete english language words
+from nltk.corpus import words
+
+#import Trie.py
+from trie import Trie, TrieNode
+
+
+class Spelling_Bee():
+    
+    def __init__(self):
+        start = time.time()
+        self._getGameData()
+        self._loadCorpus()
+        self._processCorpus()
+        end = time.time()
+        print(end-start, self.corpus_size)
+        
+    def _loadCorpus(self):
+ 
+        self.corpus = words.words()
+        self.corpus_size = len(self.corpus)
+        self._shortWordsRemoved = False
+        self._capitalsRemoved = False
+        self._corpusSearched = False
+        self._includesCenter = False
+    
+        
+        
+    def _processCorpus(self):
+        #lowers case
+        self._removeCapitals()
+        #remove short words
+        self._removeShortWords()
+        self._searchCorpus()
+        self._checkForCenter()
+       
+        #remove words that do not contain center letter
+        #remove words that are not made up of valid letters
+    
+    def _removeShortWords(self):
+# =============================================================================
+#         for word in self.corpus:
+#             if len(word) < 4:
+#                 self.corpus.remove(word)
+# =============================================================================
+        
+        self.corpus[:] = [word for word in self.corpus if len(word) >3 ]
+        self.corpus_size = len(self.corpus)
+        print('''Removed short words. corpus size now {} words
+              '''.format(
+                self.corpus_size))
+        self._shortWordsRemoved = True
+        
+    def _removeCapitals(self):
+        self.corpus[:] = [word for word in self.corpus if word.islower()]
+        self._capitalsRemoved = True
+        '''
+        Mutate exsisitng corpus to remove words that contain capitals. Words with 
+        capitals are assumed to be proper nouns and are not considered solutions.
+        
+        Exerpt Detaling Big(O) for Slicing into a list 
+        
+        Slice operations require more thought. To access the slice [a:b] of a list, we 
+        must iterate over every element between indices a and b. So, slice access is 
+        O(k)O(k), where kk is the size of the slice. Deleting a slice is O(n)O(n) for 
+        the same reason that deleting a single element is O(n)O(n): nn subsequent 
+        elements must be shifted toward the list's beginning.[1]
+        
+        [1] https://bradfieldcs.com/algos/analysis/performance-of-python-types/
+        
+        '''
+                
+    
+    def _searchCorpus(self):
+
+        self.corpus[:] = [word for word in self.corpus 
+                   if set(list(word)).issubset(self.validLetters)] 
+        #^^ https://stackoverflow.com/questions/1207406/how-to-remove-items-from-a-list-while-iterating
+        self.corpus_size =  len(self.corpus)
+        print('''Removed words that include characters not in the puzzel, {} 
+words remain in corpus
+              '''.format(self.corpus_size))
+        self._corpusSearched = True
+        
+    def _checkForCenter(self):
+        self.corpus[:] = [word for word in self.corpus if self.centerLetter in set(list(word))]
+        self.corpus_size =  len(self.corpus)
+        print('''Removed words that do not include center letter, {} words remain in corpus
+              '''.format(self.corpus_size))
+        self._includesCenter = True
+        
+    def getWinners(self):
+        if not self._shortWordsRemoved:
+            self._removeShortWords()
+        if not self._capitalsRemoved:
+            self._removeCapitals()
+        if not self._corpusSearched:
+            self._searchCorpus()
+        if not self._checkForCenter:
+            self._checkForCenter()
+            
+        return self.corpus
+              
+    def _getGameData(self):
+        url ='https://www.nytimes.com/puzzles/spelling-bee'
+        xpath ='/html/body/div[2]/div[2]/div[2]/script/text()'
+        
+        page = requests.get(url)
+        
+        tree = html.fromstring(page.content)
+        gameDataRaw = tree.xpath(xpath)
+        self.gameDataDict = json.loads(gameDataRaw[0][18:])
+        
+        
+        self.centerLetter = self.gameDataDict['today']['centerLetter']
+        self.validLetters = self.gameDataDict['today']['validLetters']
+        
+
+    
+class method1(Spelling_Bee):
+    
+    '''
+    loads nltk.brown corpus into a list. Iterates through the list 4 times 
+    checking for each soultion critera. Removes words when they fail a criteria.
+    '''
+    
+    def __init__(self):
+        start = time.time()
+        self._getGameData()
+        self._loadCorpus()
+        self._processCorpus()
+        end = time.time()
+        print(end-start, len(self.corpus))
+        
+    def _loadCorpus(self):
+ 
+        self.corpus = words.words()
+        self.corpus_size = len(self.corpus)
+        self._shortWordsRemoved = False
+        self._capitalsRemoved = False
+        self._corpusSearched = False
+        self._includesCenter = False
+    
+        
+        
+    def _processCorpus(self):
+        #lowers case
+        self._removeCapitals()
+        #remove short words
+        self._removeShortWords()
+        #remove words that are not made up of valid letters
+        self._searchCorpus()
+        #remove words that do not contain center letter
+        self._checkForCenter()
+
+      
+    def _removeShortWords(self):
+         for word in self.corpus[::-1]: # iterate through list backwards to avoid messing up index
+             if len(word) < 4:
+                 self.corpus.remove(word)
+         self._shortWordsRemoved = True
+        
+    def _removeCapitals(self):
+        for word in self.corpus[::-1]:
+            if not word.islower():
+                self.corpus.remove(word)
+        self._capitalsRemoved = True
+        
+    def _corpusSearched(self):
+        for word in self.corups[::-1]:
+             if not set(list(word)).issubset(self.validLetters):
+                 self.corpus.reomve(word)
+        self._corpusSearced = True
+        
+    def _checkForCenter(self):
+        for word in self.corpus[::-1]:
+            if not self.centerLetter in set(list(word)):
+                self.corpus.remove(word)
+        self._includesCenter = True
+                                   
+    def _searchCorpus(self):
+        # =============================================================================
+        #         for word in self.corpus:
+        #             current = set(list(word))
+        #             if not current.issubset(self.validLetters):
+        #                 self.corpus.remove(word)
+        # =============================================================================
+        self.corpus[:] = [word for word in self.corpus 
+                   if set(list(word)).issubset(self.validLetters)] 
+        #^^ https://stackoverflow.com/questions/1207406/how-to-remove-items-from-a-list-while-iterating               
+        self.corpus_size =  len(self.corpus)
+        print('''Removed words that include characters not in the puzzel, {} 
+        words remain in corpus
+              '''.format(self.corpus_size))
+        self._corpusSearched = True
+        
+    def _checkForCenter(self):
+        self.corpus[:] = [word for word in self.corpus if self.centerLetter in set(list(word))]
+        self.corpus_size =  len(self.corpus)
+        print('''Removed words that do not include center letter, {} words remain in corpus
+              '''
+              .format(self.corpus_size))
+        self._includesCenter = True
+    
+    
+class method2(Spelling_Bee):
+            
+    def __init__(self):
+        start = time.time()
+        self._getGameData()
+        self._loadProccessCorpus()
+        #self._trie()
+        end = time.time()
+        print(end-start, len(self.corpus))
+        
+    def _valid_word(self, word):
+        length = len(word) > 3
+        capital = word.islower()
+        characters = set(list(word)).issubset(self.validLetters)
+        center = self.centerLetter in set(list(word))
+        
+        return length and capital and characters and center
+        
+    def _loadProccessCorpus(self):
+        self.corpus = []
+        for word in words.words():
+            if self._valid_word(word):
+                self.corpus.append(word)
+       
+
+
+class method3(Spelling_Bee):
+    '''
+    nltk.brown corpus filtered on saving to trie data type
+    '''
+        
+    def __init__(self):
+        start = time.time()
+        self._getGameData()
+        self._make_trie()
+        self._loadCorpus()
+        #self._trie()
+        end = time.time()
+        print(end-start)
+    
+        
+        
+    def _make_trie(self):
+        self.trie = Trie()
+    
+    def _loadCorpus(self):
+        for word in words.words():
+            if len(word) > 3 and set(list(word)).issubset(self.validLetters):
+                self.trie.insert(word)
+                
+
+
+        
+class Pro(Spelling_Bee):    
+        
+    def __init__(self):
+        self._getGameData()
+      
+    
+    
+    def _getGameData(self):
+        start = time.time()
+        url ='https://www.nytimes.com/puzzles/spelling-bee'
+        xpath ='/html/body/div[2]/div[2]/div[2]/script/text()'
+        
+        page = requests.get(url) #get request from url
+        
+        tree = html.fromstring(page.content)  #parse
+        gameDataRaw = tree.xpath(xpath) #xpath extract, game data a json
+        self.gameDataDict = json.loads(gameDataRaw[0][18:]) #extracts as dict
+        
+        
+        self.answers = self.gameDataDict['today']['answers']
+        self.centerLetter = self.gameDataDict['today']['centerLetter']
+        self.validLetters = self.gameDataDict['today']['validLetters']
+        self.pangrams = self.gameDataDict['today']['pangrams']
+        end = time.time()
+        print(end-start, len(self.answers))
+        
+        
+    def getWinners(self):
+        return self.answers
+    
+    
+if __name__ == "__main__":
+    base = Spelling_Bee()
+    #m1 = method1()
+    m2 = method2()
+    m3 = method3()
+    p = Pro()
---
+++ version2/trie.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 12 12:24:38 2020
+
+@author: drahcir1
+"""
+
+
+class TrieNode:
+
+    def __init__(self, character):
+        '''
+        Trie Node class. Keeps children as a list of TrieNode objects
+        '''
+        self.character = character
+        self.children = []
+        self.isEndOfWord = False
+   
+
+
+class Trie:
+
+    def __init__ (self):
+        
+        '''
+        Trie data structure. 
+        '''
+        self.root = TrieNode("")
+        self.root.isEndOfWord = True
+        self.number_nodes = 1
+
+    def insert(self, word):
+        '''
+        adds a word from the dictonary to the trie. Starts from the first 
+        character in the word. Creates a new nodes as needed. 
+        '''      
+        node = self.root #starts with ''
+       # print(word)
+        for letter in word: 
+            for child in node.children:
+                
+                if child.character == letter:
+                    node = child
+                    break
+            else:
+                child = TrieNode(letter)
+                node.children.append(child)
+                self.number_nodes += 1
+                node = child
+
+
+        node.isEndOfWord = True
+
+
+
+    def search(self, word):
+        
+        '''
+        iterates through each character in the word. If the following character
+        is a child, that becomes the new node.  if you get to the end of the
+        word then and the isEndOfWord == True returns true, else returns false
+        '''
+        
+        node = self.root #starts with ''
+        for letter in word: 
+            for child in node.children:
+                if child.character == letter:
+                    if node.isEndOfWord == True:
+                        node = child
+                    break
+            else:
+                return False
+
+        return node.isEndOfWord
+
+
+
+    def depthFirstSearch(self, node, word, includesKey, charstring, wordlist = []):
+        if node.character not in charstring:
+            return
+        word += node.character
+        includesKey = includesKey or node.character == charstring[0]
+        if includesKey and node.isEndOfWord:
+            wordlist.append(word)
+        for child in node.children:
+            self.depthFirstSearch(child, word, includesKey, charstring, wordlist)
+        return wordlist
+
+    # def getValidWords(self, charstring):
+    #     return self.depthFirstSearch(self.root, '', False, charstring)
+
+    def getValidWords(self, charstring):
+        """
+        Returns list of all words in trie containing *only* letters in
+        charstring. Letters can occur 1 or more times. All words must
+        contains charstring[0] (key).
+        """
+        
+        def dfs(node, word, includesKey, charstring, wordlist=[]):
+            if node.character not in charstring:
+                # Invalid character, so stop building the word
+                return
+            word += node.character
+            includesKey = includesKey or node.character == charstring[0]
+            if includesKey and node.isEndOfWord and len(word) >= 4:
+                # completed word includes the key
+                wordlist.append(word)
+                # print(word)
+            for child in node.children:
+                dfs(child, word, includesKey, charstring, wordlist)
+            return sorted(wordlist,key=len, reverse=True)
+
+        return dfs(self.root, "", False, charstring)
